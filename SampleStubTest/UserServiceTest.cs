39d364624d6945ad6a3b5bb26b65bdbb79067a47
Wirachai <wirachai@iconext.co.th>
2015-11-23 17:08:24 +0700

    Use moq framework

--- SampleStubTest/UserServiceTest.cs
+++ SampleStubTest/UserServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using SampleStub;
 using SampleStub.Models;
 using System;
@@ -11,18 +12,20 @@ namespace SampleStubTest
     [TestFixture]
     public class UserServiceTest
     {
+        private string validUser = "username";
+        private string validPass = "password";
         [Test]
         public void IsValidLogin_ShouldReturnTrue_WhenExistAccount()
         {
             // arrange
-            UserRepository_Stub stub = new UserRepository_Stub();
-            stub.ShouldBeValid = true;
+            var mock = new Mock<IUserRepository>();
+            mock.Setup(x => x.IsExist(validUser, validPass)).Returns(true);
 
             UserService service = new UserService();
-            service.UserRepository = stub;
+            service.UserRepository = mock.Object;
 
             // act
-            LoginModel model = new LoginModel() { Username = "username", Password = "password" };
+            LoginModel model = new LoginModel() { Username = validUser, Password = validPass };
             bool result = service.IsValidLogin(model);
 
             // assert
